a8776dfee0fea4d675f0cf78a82b8e72ab0364e1
rundna <run.dna@gmail.com>
2018-01-02 20:11:13 +0100

    first commit

---
+++ src/main/java/com/kodilla/kodillacourse2/Kodillacourse2Application.java
@@ -0,0 +1,12 @@
+package com.kodilla.kodillacourse2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kodillacourse2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Kodillacourse2Application.class, args);
+	}
+}
