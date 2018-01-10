d330da03e3ce3c5a352ff001dd029555e1bd41d3
rundna <run.dna@gmail.com>
2018-01-10 20:46:04 +0100

    Module 6.1

---
+++ src/test/java/com/kodilla/kodillacourse2/Kodillacourse2ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kodilla.kodillacourse2;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Kodillacourse2ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
